f7b9478e7aa8a6840a6c2adede4a92d414d419ca
ahmadhian.daffa <ahmadhian.daffa@gmail.com>
2024-12-17 08:50:24 +0700

    Refactor injected.js

--- injected.js
+++ injected.js
@@ -1,17 +1,16 @@
 import { rkaBelanja } from "./rka/rkaBelanja";
+import { rkaSKPD } from "./rka/rkaSKPD";
 import { apply_style, merge_cell,set_width, rkaBelanjaHeader } from "./rka/rkaBelanjaHeader";
-// import "./xlsx.js";
-// import "./FileSaver.js";
-// import  "xlsx";
+
 (function () {
     const oldXHROpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
         this.addEventListener("load", function () {
             const targetURLs = [
-                '/api/renja/laporan/rkaBelanjaSkpd',
-                '/api/renja/laporan/rkaPendapatanSkpd',
-                '/api/renja/laporan/rkaRekapitulasiBelanjaSkpd',
-                '/api/renja/laporan/rkaPembiayaanSkpd'
+                '/api/renja/laporan/rkaBelanjaSkpd', // SKPD
+                // '/api/renja/laporan/rkaPendapatanSkpd',
+                '/api/renja/laporan/rkaRekapitulasiBelanjaSkpd', // Belanja
+                // '/api/renja/laporan/rkaPembiayaanSkpd'
             ];
 
             if (targetURLs.some(url => this.responseURL.includes(url))) {
@@ -24,27 +23,47 @@ import { apply_style, merge_cell,set_width, rkaBelanjaHeader } from "./rka/rkaBe
                 window.postMessage({ type: "INTERCEPTED_DATA", data }, "*");
                 let list = JSON.parse(data.response)["data"];
                 console.log(list);
+                console.log(data.url);
+                
 
-                let worksheetData = rkaBelanja(list);
-                let worksheetHeader = rkaBelanjaHeader()
-                let worksheetFinal = [...worksheetHeader, ...worksheetData];
+                let wsData = null;
+                let wsHeader = null;
+                let filename = null;
+                // console.log(data.url.split("/").pop());
+                switch(data.url.split("/").pop()){ // Get last url component
+                    case "rkaBelanjaSkpd":
+                        wsData = rkaBelanja(list);
+                        wsHeader = rkaBelanjaHeader();
+                        filename = "Laporan Belanja"
+                        console.log("SKPD");
+                        break;
+                    case "rkaRekapitulasiBelanjaSkpd":
+                        wsData = rkaBelanja(list);
+                        wsHeader = rkaBelanjaHeader();
+                        filename = "Laporan SKPD"
+                        console.log("Belanja");
+                        break;
+                    default:
+                        console.log("Not implemented yet");
+                }
+                
+                let wsFinal = [...wsHeader, ...wsData];
 
                 var XLSX = require("xlsx-js-style");
                 let wb = XLSX.utils.book_new();
                 wb.Props = {
-                    Title: "Laporan",
-                    Subject: "Testing",
+                    Title: "Laporan SIPD RI",
+                    Subject: "Laporan SIPD RI",
                     Author: "SIPD RI",
                 };
 
-                const ws = XLSX.utils.aoa_to_sheet(worksheetFinal);
+                const ws = XLSX.utils.aoa_to_sheet(wsFinal);
                 merge_cell(ws);
-                set_width(ws)
-                
-                XLSX.utils.book_append_sheet(wb, ws, "Template");
-                apply_style(ws)
-                
-                XLSX.writeFile(wb, "Test.xlsx");
+                set_width(ws);
+                apply_style(ws);
+
+                XLSX.utils.book_append_sheet(wb, ws, "Rekap");
+                XLSX.writeFile(wb, filename + ".xlsx");
                 
             }
         });
